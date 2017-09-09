14ce167aea919327d6c6285965f485f2010c0dbe
parabellum199316 <parabellum199316@gmail.com>
2017-09-09 11:53:03 +0300

    Add pods

---
+++ TestTask/Recorder.swift
@@ -0,0 +1,13 @@
+//
+//  Recorder.swift
+//  TestTask
+//
+//  Created by ParaBellum on 9/9/17.
+//  Copyright © 2017 ParaBellum. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
+class Recorder:AVAudioRecorderDelegate
