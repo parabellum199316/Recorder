16c81bdd3e4b03ba4f7794c86f4bce40d6a5156e
parabellum199316 <parabellum199316@gmail.com>
2017-09-18 10:43:41 +0300

    PlayVC

--- TestTask/PlayTableViewController.swift
+++ TestTask/PlayTableViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 import RealmSwift
 
 class PlayTableViewController: UITableViewController {
-    
-    
+    var player = Player.shared
+    let realm = try! Realm()
     var sections:[DueDate]!
     fileprivate let cellID = "RecordCell"
-    //fileprivate var records:Results<Record>!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        }
-       
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
-        let realm = try! Realm()
+        getLatestData()
+        tableView.reloadData()
+        
+        
+    }
+    func getLatestData(){
         
         sections = realm.objects(DueDate.self).sorted(by: {(lhs,rhs) -> Bool in
             if lhs.date > rhs.date{
@@ -29,22 +34,18 @@ class PlayTableViewController: UITableViewController {
                 return false
             }
         })
-
-        tableView.reloadData()
-        
-        
     }
     
     
     // MARK: - Table view data source
-    
+
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section].dateString
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         
-        return sections.count
+        return sections != nil ? sections.count : 0
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -66,49 +67,46 @@ class PlayTableViewController: UITableViewController {
     }
     
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
     
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
     
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let dueDate =  sections[indexPath.section]
+            let record = dueDate.records[indexPath.row]
+            //Remove record from section
+            try! realm.write {
+                dueDate.records.remove(objectAtIndex: indexPath.row)
+            }
+            let url = record.url
+            //Delete file
+            try? FileManager.default.removeItem(at: url)
+            
+            //Delete row
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            //If it was last record in section - delete section
+            if dueDate.records.count == 0 {
+                sections.remove(at: indexPath.section)
+                tableView.deleteSections(IndexSet(arrayLiteral:indexPath.section), with: .fade)
+                try! realm.write {
+                    realm.delete(dueDate)
+                }
+            }
+        }
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! RecordTableViewCell
+        player.playFrom(selectedCell: cell)
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
