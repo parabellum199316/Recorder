72ac4282c3334533f2bc267309679e609a20634f
parabellum199316 <parabellum199316@gmail.com>
2017-09-18 10:41:57 +0300

    Player Class

--- TestTask/Player.swift
+++ TestTask/Player.swift
@@ -1,3 +1,4 @@
+
 //
 //  Player.swift
 //  TestTask
@@ -7,3 +8,54 @@
 //
 
 import Foundation
+import AVFoundation
+
+class Player:NSObject {
+    
+    static let shared = Player()
+    override private init() {
+        super.init()
+        setupAudioSession()
+        
+    }
+    
+    private var audioSession = AVAudioSession.sharedInstance()
+    private var player:AVAudioPlayer!
+    
+    weak var cell:RecordTableViewCell!
+    
+    private func setupAudioSession(){
+    try! audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+    }
+    
+    
+    func playFrom(selectedCell:RecordTableViewCell){
+        cell = selectedCell
+        do {
+            player = try AVAudioPlayer(contentsOf:selectedCell.recordURL )
+            player.delegate  = self
+            guard let player = player else { return }
+            if !cell.isRed{
+                player.volume = 1
+                player.prepareToPlay()
+                selectedCell.toggleColor()
+                player.play()
+            }else{
+                cell.toggleColor()
+                player.stop()
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        
+    }
+}
+extension Player: AVAudioPlayerDelegate{
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        cell.toggleColor()
+    }
+    
+    
+    
+}
